79263d90ac4eaa7b716db1ad152f4da8966d2cbf
PhamThiNgocHoa <ptnh20052003@gmail.com>
2023-10-16 22:04:41 +0700

    hoàn thành bài tập số 2 lab5

---
+++ BTTHSO4/Lab5_Bai2/src/main/java/com/example/lab5_bai2/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.example.lab5_bai2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URL;
+import javax.servlet.ServletException;
+
+@WebServlet("/g")
+public class HelloServlet extends HttpServlet {
+    String url = "/WellCome.jsp";
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Thực hiện chuyển hướng đến trang WellCome.jsp
+        RequestDispatcher S = getServletContext().getRequestDispatcher(url);
+        S.forward(request, response);
+    }
+
+
+
+    public void destroy() {
+    }
+}
---
+++ BTTHSO4/Lab5_Bai2/src/main/webapp/WellCome.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: LaptopUSAPro
+  Date: 10/16/2023
+  Time: 9:09 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello Servlet</title>
+
+</head>
+<body>
+<h1>WellCome to HelloServlet!</h1>
+</body>
+</html>
